d07f186cdd5dc72b77899d99e5f44b965f8ebfd0
Jennifer Garcia <jen.garcia91@gmail.com>
2020-02-05 21:09:21 -0800

    Still getting CORS issue updating frontend so i can update master and show the issue off

--- frontend/src/APIcall.js
+++ frontend/src/APIcall.js
@@ -1,5 +1,6 @@
+
+const API_URL = 'http://localhost:5000';
 function make_request(route, success, error, data={}, method='GET') {
-  const API_URL = 'http://localhost:5000';
 
   console.log(API_URL + route);
   console.log(method);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import Landingpage from './Components/landingpage';
 
-const API_URL = 'http://localhost:5000';
-
 class App extends React.Component {
 
   render() {
