2b2845cfac1054e01f440d1e9407a7c742d39623
Jennifer Garcia <jen.garcia91@gmail.com>
2020-03-31 18:23:48 -0700

    Updating Repo with minor changes

--- frontend/src/APIcall.js
+++ frontend/src/APIcall.js
@@ -3,7 +3,7 @@ const API_URL = 'http://localhost:5000';
 function make_request(route, success, error, data={}, method='GET') {
 
   console.log(API_URL + route);
-  console.log(method);
+  console.log("data is ",data);
   fetch(API_URL + route, {method: method})
   .then(res => res.json())
   .then(success, error);  
